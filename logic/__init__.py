262e16e6f4d4210277286ecc61f8538214b696e3
Lev Misyuk <l.misyuk@yandex.ru>
2021-04-29 16:05:30 +0300

    add logging
    Signed-off-by: Lev Misyuk <l.misyuk@yandex.ru>

--- logic/__init__.py
+++ logic/__init__.py
@@ -1,6 +1,10 @@
 import os
+import logging
+import logging.config
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+logging.basicConfig(filename='./logs/bot.log', format='%(asctime)s | %(levelname)s | %(name)s | %(message)s', level=logging.INFO)
 
 bot = Bot(token=os.getenv('BOT_TOKEN'))
 dp = Dispatcher(bot, storage=MemoryStorage())
