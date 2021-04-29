8078b522985760e7f4d0a9a21d84e740bfb286af
Lev Misyuk <l.misyuk@yandex.ru>
2021-04-29 22:17:11 +0300

    fix: fix logging format

--- logic/__init__.py
+++ logic/__init__.py
@@ -1,10 +1,9 @@
 import os
 import logging
-import logging.config
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-logging.basicConfig(filename='./logs/bot.log', format='%(asctime)s | %(levelname)s | %(name)s | %(message)s', level=logging.INFO)
+logging.basicConfig(filename='./logs/bot.log', format='%(asctime)s | %(levelname)s | %(module)s | %(message)s', level=logging.INFO)
 
 bot = Bot(token=os.getenv('BOT_TOKEN'))
 dp = Dispatcher(bot, storage=MemoryStorage())
