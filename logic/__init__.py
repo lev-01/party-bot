dbdd89288b0216c06e50030f1ddb33e80dae0d5a
Lev Misyuk <l.misyuk@yandex.ru>
2021-05-02 17:35:23 +0300

    fix: changed logs level

--- logic/__init__.py
+++ logic/__init__.py
@@ -3,7 +3,7 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-logging.basicConfig(filename='./logs/bot.log', format='%(asctime)s | %(levelname)s | %(module)s | %(message)s', level=logging.INFO)
+logging.basicConfig(filename='./logs/bot.log', format='%(asctime)s | %(levelname)s | %(module)s | %(message)s', level=logging.DEBUG)
 
 bot = Bot(token=os.getenv('BOT_TOKEN'))
 dp = Dispatcher(bot, storage=MemoryStorage())
